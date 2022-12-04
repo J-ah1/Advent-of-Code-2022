12dee31ad512c170c897a339c66c370007fb5a2e
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-04 14:49:53 -0500

    day2 (redone)

--- Advent of Code 2022/src/day2/PartA.java
+++ Advent of Code 2022/src/day2/PartA.java
@@ -1,5 +1,42 @@
 package day2;
 
-public class PartA {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class PartA {
+	/*
+	 * Used to obtain score based on OPP(y-axis/cols) vs US(x-axis/rows)
+	 *    R P S
+	 *  R 4 1 7
+	 *  P 8 5 2
+	 *  S 3 9 6
+	 */
+	static int[][] scoring = {
+			{4,1,7},
+			{8,5,2},
+			{3,9,6}
+	};
+	
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day2_input.txt"))) {
+    	    String line = br.readLine();
+    	    int total = 0;
+    	    while (line != null) {
+    	    	total += getScore(line);
+    	        line = br.readLine();
+    	    }
+    	    System.out.println(total);
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static int getScore(String line) {
+		// Getting the corresponding scoring indices
+		int opp = line.charAt(0) - 'A';
+		int us = line.charAt(2) - 'X';
+		return scoring[us][opp];
+	}
 }
--- Advent of Code 2022/src/day2/PartB.java
+++ Advent of Code 2022/src/day2/PartB.java
@@ -1,5 +1,42 @@
 package day2;
 
-public class PartB {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class PartB {
+	/*
+	 * Used to obtain score based on OPPONENT(y-axis/cols) vs OUTCOME(x-axis/rows)
+	 *    R P S
+	 *  L 3 1 2
+	 *  D 4 5 6
+	 *  W 8 9 7
+	 */
+	static int[][] scoring = {
+			{3,1,2},
+			{4,5,6},
+			{8,9,7}
+	};
+	
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day2_input.txt"))) {
+    	    String line = br.readLine();
+    	    int total = 0;
+    	    while (line != null) {
+    	    	total += getScore(line);
+    	        line = br.readLine();
+    	    }
+    	    System.out.println(total);
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static int getScore(String line) {
+		// Getting the corresponding scoring indices
+		int opp = line.charAt(0) - 'A';
+		int out = line.charAt(2) - 'X';
+		return scoring[out][opp];
+	}
 }
