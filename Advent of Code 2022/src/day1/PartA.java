103d2cab36959d8010137f6a36e22b7b12331678
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-04 14:24:21 -0500

    day1 (redone)

--- Advent of Code 2022/src/day1/PartA.java
+++ Advent of Code 2022/src/day1/PartA.java
@@ -1,5 +1,29 @@
 package day1;
 
-public class PartA {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class PartA {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day1_input.txt"))) {
+    	    String line = br.readLine();
+    	    int total = 0;
+    	    int currTotal = 0;
+    	    while (line != null) {
+    	    	if(line.length() == 0) {
+    	    		total = Math.max(total, currTotal);
+    	    		currTotal = 0;
+    	    	}else{
+    	    		currTotal += Integer.parseInt(line);
+    	    	}
+    	        line = br.readLine();
+    	    }
+    	    total = Math.max(total, currTotal);
+    	    System.out.println(total);
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- Advent of Code 2022/src/day1/PartB.java
+++ Advent of Code 2022/src/day1/PartB.java
@@ -1,5 +1,34 @@
 package day1;
 
-public class PartB {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+public class PartB {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day1_input.txt"))) {
+    	    String line = br.readLine();
+    	    Queue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+    	    int currTotal = 0;
+    	    while (line != null) {
+    	    	if(line.length() == 0) {
+    	    		pq.add(currTotal);
+    	    		currTotal = 0;
+    	    	}else{
+    	    		currTotal += Integer.parseInt(line);
+    	    	}
+    	        line = br.readLine();
+    	    }
+    	    int answer = 0;
+    	    for(int i = 0; i < 3; i++) {
+    	    	answer += pq.poll();
+    	    }
+    	    System.out.println(answer);
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
