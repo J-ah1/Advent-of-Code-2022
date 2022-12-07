f6f51f1de12f49778da03e9a90471b654a305007
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-07 18:51:25 -0500

    updated day 6 comments

--- Advent of Code 2022/src/day6/PartA.java
+++ Advent of Code 2022/src/day6/PartA.java
@@ -13,15 +13,19 @@ public class PartA {
 		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day6_input.txt"))) {
 			int i = br.read();
 			int start = 1;
+			// Keep track of nums in sequence (recentNums) and O(1) access seen nums (seenNums)
 			Queue<Integer> recentNums = new LinkedList<Integer>();
 			Set<Integer> seenNums = new HashSet<Integer>();
+			// Go through each char int value (no need to convert to char)
     	    while (i != -1) {
     	    	// 10 is the end line char
     	    	if(i != 10){
     	    		recentNums.offer(i);
+    	    		// If we try to add a duplicate, keep removing from the beginning until this isn't the case
 	    			while(!seenNums.add(i)) {
 	    				seenNums.remove(recentNums.poll());
 	    			}
+	    			// Break if we can form a length 4 seq of ints
 	    			if(recentNums.size() == 4) break;
     	    		start++;
     	    	}
