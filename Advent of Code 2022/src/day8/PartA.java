0a0df09af1e373b94788885dc571ba9821970723
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-08 11:57:47 -0500

    day 8

--- Advent of Code 2022/src/day8/PartA.java
+++ Advent of Code 2022/src/day8/PartA.java
@@ -3,17 +3,96 @@ package day8;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PartA {
 
+	// NTS: This goes through 6 N length passes, so O(N)
+	// Could probably re-factor this still
+	
 	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day8_input.txt"))) {
+			// Gathering all the heights and making a 2d array
     	    String line = br.readLine();
-
+    	    List<List<Integer>> heights = new ArrayList<List<Integer>>();
     	    while (line != null) {
-    	    	
+    	    	List<Integer> addList = new ArrayList<Integer>();
+    	    	for(char c : line.toCharArray()) {
+    	    		addList.add(Integer.parseInt(Character.toString(c)));
+    	    	}
+    	    	heights.add(addList);
     	        line = br.readLine();
     	    }
+    	    
+    	    
+    	    
+    	    // Setting up the visibility array, labeling outermost trees visible
+    	    int numRows = heights.size();
+    	    int numCols = heights.get(0).size();
+    	    int[][] isVisible = new int[numRows][numCols];
+    	    for(int x = 0; x < numCols; x++) {
+    	    	isVisible[0][x] = 1;
+    	    	isVisible[numRows-1][x] = 1;
+    	    }
+    	    for(int y = 0; y < numRows; y++) {
+    	    	isVisible[y][0] = 1;
+    	    	isVisible[y][numCols-1] = 1;
+    	    }
+    	    
+    	    // Setting if a tree is visible
+    	    // This is done by going from left-right/top-bottom for each row/col
+    	    // and checking any time a "new max height" is found
+    	    // This is also done in reverse order
+    	    int max;
+    	    // Checking each row
+    	    for(int y = 1; y < numRows-1; y++) {
+    	    	max = heights.get(y).get(0);
+    	    	// Going through each col left-right
+    	    	for(int x = 1; x < numCols-1; x++) {
+    	    		if(max < heights.get(y).get(x)) {
+    	    			max = heights.get(y).get(x);
+    	    			isVisible[y][x] = 1;
+    	    		}
+        	    }
+    	    	max = heights.get(y).get(numCols-1);
+    	    	// Going through each col right-left
+    	    	for(int x = numCols-2; x > 0; x--) {
+    	    		if(max < heights.get(y).get(x)) {
+    	    			max = heights.get(y).get(x);
+    	    			isVisible[y][x] = 1;
+    	    		}
+        	    }
+    	    }
+    	    // Checking each col
+    	    for(int x = 1; x < numCols-1; x++) {
+    	    	max = heights.get(0).get(x);
+    	    	// Going through each row left-right
+    	    	for(int y = 1; y < numRows-1; y++) {
+    	    		if(max < heights.get(y).get(x)) {
+    	    			max = heights.get(y).get(x);
+    	    			isVisible[y][x] = 1;
+    	    		}
+        	    }
+    	    	max = heights.get(numRows-1).get(x);
+    	    	// Going through each row right-left
+    	    	for(int y = numRows-2; y > 0; y--) {
+    	    		if(max < heights.get(y).get(x)) {
+    	    			max = heights.get(y).get(x);
+    	    			isVisible[y][x] = 1;
+    	    		}
+        	    }
+    	    }
+    	    
+    	    // Counting the visible trees in the isVisible arr
+    	    int total = 0;
+    	    for(int y = 0; y < numRows; y++) {
+    	    	for(int x = 0; x < numCols; x++) {
+    	    		if(isVisible[y][x] == 1) total++;
+        	    }
+    	    }
+    	    System.out.println(total);
 
     	} catch (IOException e) {
 			// TODO Auto-generated catch block
--- Advent of Code 2022/src/day8/PartB.java
+++ Advent of Code 2022/src/day8/PartB.java
@@ -3,17 +3,123 @@ package day8;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PartB {
 
+	// NTS: This also goes through 6 N length passes
+	// However, this makes comparisons on a growing list
+	// So, to my understanding, it'd be O(n^2)
+	// Could probably re-factor this still
+	
 	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day8_input.txt"))) {
+			// Gathering all the heights and making a 2d array
     	    String line = br.readLine();
-
+    	    List<List<Integer>> heights = new ArrayList<List<Integer>>();
     	    while (line != null) {
-    	    	
+    	    	List<Integer> addList = new ArrayList<Integer>();
+    	    	for(char c : line.toCharArray()) {
+    	    		addList.add(Integer.parseInt(Character.toString(c)));
+    	    	}
+    	    	heights.add(addList);
     	        line = br.readLine();
     	    }
+    	    
+    	    
+    	    
+    	    // Setting up scenicScores, the outermost is all 0 whereas the inner is 1
+    	    int numRows = heights.size();
+    	    int numCols = heights.get(0).size();
+    	    int[][] scenicScore = new int[numRows][numCols];
+    	    for(int y = 1; y < numRows-1; y++) {
+    	    	for(int x = 1; x < numCols-1; x++) {
+    	    		scenicScore[y][x] = 1;
+        	    }
+    	    }
+    	    
+    	    
+    	    
+    	    // Calculating the scenic score for each tree
+    	    // This is done by going from left-right/top-bottom for each row/col
+    	    // and constantly comparing to a prevHeights list, incrementing score accordingly
+    	    // and adding to said list
+    	    // This is also done in reverse order
+    	    int currScore;
+    	    List<Integer> prevHeights = new ArrayList<Integer>();
+    	    // Checking each row
+    	    for(int y = 1; y < numRows-1; y++) {
+    	    	prevHeights.clear();
+    	    	prevHeights.add(heights.get(y).get(0));
+    	    	// Going through each col left-right
+    	    	for(int x = 1; x < numCols-1; x++) {
+    	    		currScore = 1;
+    	    		int currHeight = heights.get(y).get(x);
+    	    		for(int i = prevHeights.size()-1;
+    	    				i > 0 && prevHeights.get(i) < currHeight;
+    	    				i--) {
+    	    			currScore++;
+    	    		}
+    	    		scenicScore[y][x] *= currScore;
+    	    		prevHeights.add(heights.get(y).get(x));
+        	    }
+    	    	prevHeights.clear();
+    	    	prevHeights.add(heights.get(y).get(numCols-1));
+    	    	// Going through each col right-left
+    	    	for(int x = numCols-2; x > 0; x--) {
+    	    		currScore = 1;
+    	    		int currHeight = heights.get(y).get(x);
+    	    		for(int i = prevHeights.size()-1;
+    	    				i > 0 && prevHeights.get(i) < currHeight;
+    	    				i--) {
+    	    			currScore++;
+    	    		}
+    	    		scenicScore[y][x] *= currScore;
+    	    		prevHeights.add(heights.get(y).get(x));
+        	    }
+    	    }
+    	    // Checking each col
+    	    for(int x = 1; x < numCols-1; x++) {
+    	    	prevHeights.clear();
+    	    	prevHeights.add(heights.get(0).get(x));
+    	    	// Going through each row left-right
+    	    	for(int y = 1; y < numRows-1; y++) {
+    	    		currScore = 1;
+    	    		int currHeight = heights.get(y).get(x);
+    	    		for(int i = prevHeights.size()-1;
+    	    				i > 0 && prevHeights.get(i) < currHeight;
+    	    				i--) {
+    	    			currScore++;
+    	    		}
+    	    		scenicScore[y][x] *= currScore;
+    	    		prevHeights.add(heights.get(y).get(x));
+        	    }
+    	    	prevHeights.clear();
+    	    	prevHeights.add(heights.get(numRows-1).get(x));
+    	    	// Going through each row right-left
+    	    	for(int y = numRows-2; y > 0; y--) {
+    	    		currScore = 1;
+    	    		int currHeight = heights.get(y).get(x);
+    	    		for(int i = prevHeights.size()-1;
+    	    				i > 0 && prevHeights.get(i) < currHeight;
+    	    				i--) {
+    	    			currScore++;
+    	    		}
+    	    		scenicScore[y][x] *= currScore;
+    	    		prevHeights.add(heights.get(y).get(x));
+        	    }
+    	    }
+    	    
+    	    // Looking through all scores and getting maxScore
+    	    int maxScore = 0;
+    	    for(int y = 0; y < numRows; y++) {
+    	    	for(int x = 0; x < numCols; x++) {
+    	    		if(maxScore < scenicScore[y][x]) maxScore = scenicScore[y][x];
+        	    }
+    	    }
+    	    System.out.println(maxScore);
 
     	} catch (IOException e) {
 			// TODO Auto-generated catch block
