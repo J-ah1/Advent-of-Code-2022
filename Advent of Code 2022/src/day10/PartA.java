9ad36e95ea5f3fa473f7b5d4229e8708b12c0097
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-11 10:56:15 -0500

    day 10

--- Advent of Code 2022/src/day10/PartA.java
+++ Advent of Code 2022/src/day10/PartA.java
@@ -5,20 +5,45 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PartA {
+	
+	private static int cycle = 1;
+	private static int x = 1;
 
 	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day10_input.txt"))) {
     	    String line = br.readLine();
-
+    	    String[] words;
+    	    int total = 0;
     	    while (line != null) {
-    	    	
+    	    	words = line.split(" ");
+    	    	if(words[0].equals("noop")) {
+    	    		total += incrementCycle(1);
+    	    		if(cycle >= 220) break;
+    	    	}else if(words[0].equals("addx")) {
+    	    		total += incrementCycle(2);
+    	    		x += Integer.parseInt(words[1]);
+    	    		if(cycle >= 220) break;
+    	    	}
     	        line = br.readLine();
     	    }
-
+    	    
+    	    System.out.println(total);
     	} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private static int incrementCycle(int times){
+		int returnVal = 0;
+		for(int i = 0; i < times; i++) {
+			if((cycle + 20) % 40 == 0) {
+				System.out.printf("%d %d \n", x, cycle);
+				returnVal += x * cycle;
+			}
+			cycle++;
+		}
+		return returnVal;
 	}
 
 }
--- Advent of Code 2022/src/day10/PartB.java
+++ Advent of Code 2022/src/day10/PartB.java
@@ -6,18 +6,51 @@ import java.io.IOException;
 
 public class PartB {
 
-	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
-    	    String line = br.readLine();
+	private static int cycle = 1;
+	private static int x = 1;
+	private final static int width = 40;
+	private final static int height = 6;
+	private static boolean[] currRow = new boolean[width];
 
+	public static void main(String[] args) {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/day10_input.txt"))) {
+    	    String line = br.readLine();
+    	    String[] words;
     	    while (line != null) {
-    	    	
+    	    	words = line.split(" ");
+    	    	if(words[0].equals("noop")) {
+    	    		incrementCycle(1);
+    	    	}else if(words[0].equals("addx")) {
+    	    		incrementCycle(2);
+    	    		x += Integer.parseInt(words[1]);
+    	    	}
     	        line = br.readLine();
     	    }
-
     	} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	private static void drawRow() {
+		for(boolean pixel : currRow) {
+			if(pixel) {
+				System.out.print("#");
+			}else {
+				System.out.print(".");
+			}
+		}
+		System.out.println();
+	}
+	
+	private static void incrementCycle(int times){
+		for(int i = 0; i < times; i++) {
+			int pixelPos = (cycle-1)%width;
+			// what happens when x is negative or greater than 40?
+			currRow[pixelPos] = pixelPos >= x-1 && pixelPos <= x+1;
+			if(cycle%width == 0) drawRow();
+			cycle++;
 		}
 	}
 
