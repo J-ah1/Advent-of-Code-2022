8b715e30cd589c993da8690a98ca829c0a796992
Joshua Almonte <JoshAlmonte10@gmail.com>
2022-12-14 17:02:44 -0500

    created placeholders for future days

---
+++ Advent of Code 2022/src/day18/PartB.java
@@ -0,0 +1,24 @@
+package day18;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PartB {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
+    	    String line = br.readLine();
+
+    	    while (line != null) {
+    	    	
+    	        line = br.readLine();
+    	    }
+
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Advent of Code 2022/src/day24/PartA.java
@@ -0,0 +1,24 @@
+package day24;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PartA {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("Inputs/dayX_input.txt"))) {
+    	    String line = br.readLine();
+
+    	    while (line != null) {
+    	    	
+    	        line = br.readLine();
+    	    }
+
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
